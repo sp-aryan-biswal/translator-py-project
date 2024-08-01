23c4142032209741acffd78c4382bb08b6f7f693
S P Aryan Biswal <141744489+sp-aryan-biswal@users.noreply.github.com>
2024-08-01 20:26:19 +0530

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,24 @@
+# To install necessary modules, run 'pip install -r <path to requirements.txt>'
+import googletrans as gtrans
+import speech_recognition as sr
+import gtts as tts
+import playsound as play
+
+# print(gtrans.LANGUAGES)
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak Now!")
+    voice = recognizer.listen(source)
+    text = recognizer.recognize_google(voice, language="en")
+
+    print(text)
+
+translator = gtrans.Translator()
+translation = translator.translate(text, dest="ru")
+print(translation.text)
+
+converted_audio = tts.gTTS(translation.text, lang="ru")
+converted_audio.save("translated.mp3")
+play.playsound("translated.mp3")
